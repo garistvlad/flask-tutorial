b3423f71791c8e9ec50db736c77ef52e25ad5e96
garistvlad <vv.garist@gmain.com>
2022-07-30 17:42:04 +0300

    added auth blueprint & templates

---
+++ blog_app/__init__.py
@@ -0,0 +1,35 @@
+import os
+from typing import Dict, Optional
+
+from flask import Flask
+
+
+def create_app(test_config: Optional[Dict] = None) -> Flask:
+    """Create and configure the app"""
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "blog_app.sqlite")
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/")
+    def index():
+        return "OK"
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    return app
