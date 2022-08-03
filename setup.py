48f499d6c7cf2b44c507fb8a4a82a7c1e887800e
garistvlad <vv.garist@gmain.com>
2022-08-04 02:42:19 +0300

    added bootstrap forms

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+APP_NAME = "blog_app"
+
+
+setup(
+    name=APP_NAME,
+    version="1.0.0",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "flask",
+    ]
+)
